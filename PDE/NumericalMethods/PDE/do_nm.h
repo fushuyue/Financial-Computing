e41782882cf2af6e9e069be795a236765b745608
fushuyue <fushuyue_apply@hotmail.com>
2016-04-09 23:23:41 -0500

    interfac filee

---
+++ PDE/NumericalMethods/PDE/do_nm.h
@@ -0,0 +1,3 @@
+double ** do_euler_explicit(int N, int M, float r, double sigma, double K, double S_max, double T);
+double ** do_euler_implicitet(int N, int M, float r, double sigma, double K, double S_max, double T);
+double ** do_crank_nicolson(int N, int M, float r, double sigma, double K, double S_max, double T);
